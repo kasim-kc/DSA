3669f42500d14e9721c73cb360edcc1256aba210
kasim-kc <kasimsactive1@gmail.com>
2023-10-11 16:02:22 +0530

    are commits independent of branches

---
+++ Striver Cpp Basics/branch.cpp
@@ -0,0 +1,5 @@
+///  Branch is a pointer that points to the snapshot
+
+// A branch has a trail of commits and its branch specific
+// On creating a new branch it has its own trail of commits that is independent to other branches.. Merge and all to be  studied later
+
