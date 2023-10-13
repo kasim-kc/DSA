4bdfcb124e97522bb5918bf26d1e6749e2e692b3
kasim-kc <kasimsactive1@gmail.com>
2023-10-13 08:56:09 +0530

    Added stacks and queues

--- Striver Cpp Basics/cppstl.cpp
+++ Striver Cpp Basics/cppstl.cpp
@@ -135,3 +135,98 @@ vector<int> copy(2, 50);
 
 
 // A demo line to check success
+
+// +++++++++++++ DEQUE CONTAINER ++++++++++++
+
+/* Declaration :*/ deque<int> dq;
+
+// It works similar to list
+/*
+    dq.push_back(1); 
+    dq.emplace_back(2);
+    dq.push_front(4);
+    dq.emplace_front(3);
+
+    // dq.pop_back();   // Removes the end element 
+    // dq.pop_front();  // Removes the element at the starting of deque..
+
+    // rest functions are same as of vector
+    // begin, end, rbegin, rend, clear, insert, size, swap..    
+*/
+
+
+// ++++++++++ STACK ++++++++++
+
+/* Declaration: */  stack<int> st;    // stack follows LIFO Principle
+/*
+    st.push(1);
+    st.push(2);      // The elements automatically get pushed at the front
+    st.push(3);      // There is no indexing in stack. has only three operations: push, pop, top take O[1]..
+    st.push(3);
+    st.emplace(5);     // {5,3,3,2,1}  how a stack could look if it was an array
+
+    cout << st.top();     // returns the last element that was pushed or say the element at first position]
+
+    st.pop();   // It will remove the last element that was pushed
+
+    cout << st.size()   // It  will return the number of elements pushed/present in stack
+
+    cout << st.empty()  // a boolean function that checks if a stack is empty..
+
+    stack<int> st1, st2;
+    st1.swap(st2);         // Replaces the contents of s1 with s2 and s2 with s1.. can say same elements under diff. name    
+*/
+
+
+// ++++++++++ QUEUES  ++++++++
+/*Declaration :*/  queue<int> q;
+// It follows FIFO principle which is First In First Out
+
+/*
+    q.push(1);  
+    q.push(2);  
+    q.emplace(4);  {1,2,4}     // pushes element at the back which means the element which was pushed first will remain at first position
+
+    q.back() += 5;   // Adds 5 to the element that was last added to the queue
+
+    cout << q.back() ; // prints the value of the last element..
+
+    cout << q.front();  // prints the first element to be pushed or say the element currently at the first position
+
+    q.pop(); // it will remove the first element of the queue
+
+    // size swap empty same as stack
+
+    // all the operations in queue take constant time..
+*/
+
+// ++++++ Priority QUEUE ++++++++
+/* Declaration :*/ priority_queue<int> pq;    // MAXXXXXXXXX HEAP
+
+// The difference comes in placing 0f the elements where the elements with higher value are given high priority and are at the front().\
+// When imagined as an array or list, the elements are to be sorted in a descending order
+// lexicographically largest string.. -> dictionary type se arranged
+/*
+    pq.push(5);
+    pq.push(2);
+    pq.push(8);
+    pq.emplace(3);    // {8,5,3,2}
+
+    cout << pq.top();   // prints the front/ top i.e, 8
+
+    pq.pop();          // removes the top element
+    
+    // The data is not maintained in a linear structure.. A tree data structure is maintained behind it
+
+    // size swap empty same as others
+
+    
+
+    // To store elements in a priority queue in a Ascending order i.e, smallest element at top --->> MINIMUM HEAP
+
+    // ++++++++ priority_queue<int, vector<int>, greater<int>> pq;   // Declaration of MINIMUM HEAP Priority Queue  ++++++++
+
+    // push, pop --> log n
+    // top --> O(1) time
+
+*/
